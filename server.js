1103062b2ed51feaeb27933604d3369f935b157f
Raghuveer Adari <raghuveer.adari@oracle.com>
2020-04-14 19:58:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,3 @@ app.use('/static', express.static('res'))
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
-
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
