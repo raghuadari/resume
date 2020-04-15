175ab5a6fa5d25d39d5362722eda8aa4c4229fff
Raghuveer Adari <raghuveer.adari@oracle.com>
2020-04-15 12:18:58 +0530

    Card title alignment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var path    = require("path");
+var path = require("path");
 
 app.use('/static', express.static('res'))
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(8080, () => {
-  console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!')
 });
